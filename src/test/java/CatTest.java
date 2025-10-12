081d886c9921c60feb6795b53f59d83f92c85f3f
Julia <yuliamarche@gmail.com>
2025-10-12 20:17:54 +0300

    Sprint_5

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -3,8 +3,9 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -19,7 +20,15 @@ public class CatTest {
     public void getCatSoundIsCorrect(){
         Cat cat = new Cat(feline);
         String expectedSound = "Мяу";
-        assertEquals(expectedSound, cat.getSound(), "Метод должен возвращать 'Мяу'");
+        assertEquals(expectedSound, cat.getSound(),"Должен возвращаться аргумент 'Мяу'");
+    }
+
+    @Test
+    public void getFoodReturnsFoodList() throws Exception{
+        Cat cat = new Cat(feline);
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expectedFood);
+        assertEquals(expectedFood,cat.getFood(),"Должен возвращаться список: 'Животные', 'Птицы', 'Рыба'");
     }
 
 }
